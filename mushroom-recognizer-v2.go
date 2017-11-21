dc70e295d25d47a6b958f657706e2e36a27ef91e
Denis Godovanets <erami.ukr@gmail.com>
2017-11-21 22:47:50 +0200

    Partially implemented receiveFile function

--- mushroom-recognizer-v2.go
+++ mushroom-recognizer-v2.go
@@ -2,49 +2,76 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"regexp"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+// Closes request with error if any errors occured
+func anyErrors(error e, w http.ResponseWriter) bool {
+	if e != nil {
+		http.Error(w, e.Error(), http.StatusInternalServerError)
+		return true
+	} else {
+		return false
+	}
+}
+
+// Generates random filename with length 32 with provided extension
+// Takes salt from cfg file, takes integer seed as last insert in database
+func generateFileName(fileExtension string) string {
+
+}
+
+// Saves file and returs its url and error
+func receiveFile(r *http.Request) (string, error) {
+	// Read file from request
+
 	// the FormFile function takes in the POST input id file
 	file, header, err := r.FormFile("file")
-
 	if err != nil {
-		fmt.Fprintln(w, err)
-		return
+		return _, err
 	}
 
 	defer file.Close()
 
-	dir, err := os.Getwd()
+	// Save file to filesystem
+
+	dir, err := os.Getwd() // Get current dir
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
-		return
+		return _, err
 	}
 
-	out, err := os.Create(dir + "\\tmp\\uploadedfile")
+	fileExtensionRegexp := regexp.MustCompile("(.jpg)|(.png)")
+	fileExtension := fileExtensionRegexp.FindString(header.Filename)
+
+	if fileExtension == "" {
+		return _, errors.New("File extension must be .jpg or .png only")
+	}
+
+	path := dir + "\\imgs\\" + filename
+	out, err := os.Create(path)
 	if err != nil {
-		//fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
-		fmt.Fprintf(w, "Err: "+err.Error())
-		return
+		return _, err
 	}
 
 	defer out.Close()
-
 	// write the content from POST to the file
 	_, err = io.Copy(out, file)
 	if err != nil {
-		fmt.Fprintln(w, err)
+		return _, err
 	}
 
-	//fmt.Fprintf(w, "File uploaded successfully : ")
-	fmt.Fprintf(w, header.Filename)
+	return path, nil
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 }
 
